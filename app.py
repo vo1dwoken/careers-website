85ee03e9473a7a25e338089da8111dac53881c73
Andrii <ilchenko.andrii15@gmail.com>
2023-09-24 17:12:58 +0200

    Add list of jobs dictionaries and api route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, jsonify
 import dotenv
 import os
 
@@ -11,9 +11,22 @@ app.config["FLASK_ENV"] = os.getenv("FLASK_ENV")
 app.config["FLASK_DEBUG"] = os.getenv("FLASK_DEBUG")
 
 
+JOBS = [
+    {"id": 1, "title": "Data Analyst", "location": "Warsaw", "salary": "6000 zł"},
+    {"id": 2, "title": "Backend Engineer", "location": "Warsaw", "salary": "8000 zł"},
+    {"id": 3, "title": "Frontend Engineer", "location": "Remote", "salary": "7000 zł"},
+    {"id": 4, "title": "Fullstack Engineer", "location": "Wrocław"},
+]
+
+
 @app.route("/")
 def index():
-    return render_template("home.html")
+    return render_template("home.html", jobs=JOBS)
+
+
+@app.route("/api/jobs")
+def json():
+    return jsonify(JOBS)
 
 
 if __name__ == "__main__":
